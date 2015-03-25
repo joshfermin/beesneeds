5cca43b42f6db37e5624428087bd5007c989171e
joshfermin <joshfermin@gmail.com>
2015-03-25 12:55:33 -0600

    fixed export in xls file in datareports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,7 +22,7 @@ class ReportsController < ApplicationController
     respond_to do |format|
       format.html
       format.csv { send_data @reports.to_csv }
-      format.xls #{ send_data @reports.to_csv(col_sep: "\t") }
+      format.xls { send_data @reports.to_csv(col_sep: "\t") }
     end
   end
 
