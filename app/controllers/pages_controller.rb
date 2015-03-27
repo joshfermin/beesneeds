65a2c99fbded78b590dcae38ade0a60830e09742
joshfermin <joshfermin@gmail.com>
2015-03-26 18:02:38 -0600

    Revert "merging dev master"
    This reverts commit 736003728aea4633da91291538c0dc3e1498ecca, reversing
    changes made to 5cca43b42f6db37e5624428087bd5007c989171e.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,9 @@
 # Handles HTTP requests for Pages
 class PagesController < ApplicationController
-	skip_before_action :authenticate_user!
   def home
   end
 
   def stat
    @data = Report.where("date(created_at) > ?", 15.days.ago).group("date(created_at)").count.values.reverse!
-  end
-
-  def placeholder
   end
 end
