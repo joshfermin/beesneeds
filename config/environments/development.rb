3adb9788243865c016ce88e2e075805ecc33dd1e
joshfermin <joshfermin@gmail.com>
2015-03-31 21:31:37 -0600

    fix for email? maybe?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Beesneeds::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'beesneeds-dev.colorado.edu' }
 #  config.action_mailer.delivery_method = :smtp
  ActionMailer::Base.smtp_settings = {  
   :address              => "smtp.gmail.com",  
