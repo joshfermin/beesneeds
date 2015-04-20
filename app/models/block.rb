ce27561f6d307ea1dfe7babaad4914d80bf05977
joshfermin <joshfermin@gmail.com>
2015-04-19 23:14:06 -0600

    made it so 555555 is a valid block number for alex testing

--- app/models/block.rb
+++ app/models/block.rb
@@ -5,7 +5,7 @@ class Block < ActiveRecord::Base
 	validates_uniqueness_of :block_number
 	has_one :info
 	validates :nickname, :presence => true
-	validates_format_of :block_number, with: /\A153...\z/, :message => "invalid--please try again and note that you may not register blocks from previous seasons of The Bees' Needs. If you're trying to report an observation on a bee block from 2013 or 2014, please use the Submit Photo option." 
+	validates_format_of :block_number, with: /((\A153...\z)|(\A555555\z))/, :message => "invalid--please try again and note that you may not register blocks from previous seasons of The Bees' Needs. If you're trying to report an observation on a bee block from 2013 or 2014, please use the Submit Photo option." 
 
 	def self.to_csv(options = {})
 		CSV.generate(options) do |csv|
